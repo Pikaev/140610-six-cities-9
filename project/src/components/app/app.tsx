46e943fd9e6310747b649d4e3c2bccfff9afee89
Sergey <yapikay@ya.ru>
2022-02-24 05:07:25 +0300

    Adds application components

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../mainScreen/mainScreen';
+
+type AppScreenProps = {
+  placeCount: number;
+}
+
+function App({placeCount}: AppScreenProps): JSX.Element {
+  return <MainScreen placeCount={placeCount} />;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  PLACE_COUNT: 256,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      placeCount={Setting.PLACE_COUNT}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
